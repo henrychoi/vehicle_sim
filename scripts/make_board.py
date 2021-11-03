dfdd713008cb4ce2c4c850e0c26d1e15f3c6fc40
Henry <henry@rhom.me>
2021-11-03 06:31:44 -0700

    Use Aruco board; calculating R and T; still have to check correctness

--- scripts/make_board.py
+++ scripts/make_board.py
@@ -1,34 +1,42 @@
-import cv2
+import cv2 as cv
 import getopt
 import sys
 
 def main():
     verbose = False
-    N = 1
+    N = 2
     M = 3
-    _usage = "{-o, --output} [output image file] " + \
-        "{-N, --rows} [number of rows for the grid] " + \
-        "{-M, --columns} [number of columns for the grid] "
+    len = 0.15 # [m]
+    gap = 0.01 # [m]
+    margin = 8 # [pixel]
+    _usage = "{-o, --output} output image file\n" + \
+        "{-N, --rows} number of rows for the grid\n" + \
+        "{-M, --columns} number of columns for the grid\n" + \
+        "--length length of 1 ARUCO marker [m]]\n" + \
+        "--gap gap between markers [m]\n" + \
+        "--margin minimum margin in the output image [pixel]\n" 
     options, remainder = getopt.getopt(sys.argv[1:], 'o:N:M:v',
-        ['output=', 'rows=', 'columns=', 'verbose'])
+        ['output=', 'rows=', 'columns=', 'length=', 'gap=', 'verbose'])
     for opt, arg in options:
         if opt in ('-o', '--output'): output_filename = arg
-        elif opt in ('-N', '--rows'): N = arg
+        elif opt in ('-N', '--rows'): N = int(arg)
+        elif opt in ('-M', '--columns'): M = int(arg)
+        elif opt=='--margin': margin = int(arg)
+        elif opt=='--length': len = float(arg)
+        elif opt=='--gap': gap = float(arg)
         elif opt in ('-v', '--verbose'): verbose = True
-        else:
-            print("Invalid argument " + opt)
-            sys.exit(_usage)
+        else: sys.exit(_usage)
 
     # Follow https://gregorkovalcik.github.io/opencv_contrib/tutorial_aruco_board_detection.html
-    # TODO: translate to python:
     # See https://docs.opencv.org/4.5.3/dc/df7/dictionary_8hpp.html
-    # dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50) # or just 0, for the simplest one
-    # cv::aruco::GridBoard board = cv::aruco::GridBoard::create(M, N, 0.12, 0.02, dict);
-    # board.draw(outSize, cvmat)
-    # cv::imwrite(output_filename, cvmat); # save img to the outfile
-    # if verbose: # show the generated grid board
-    #   cv::imshow(output_filename, cvmat);
-    #   cv::waitKey()
+    dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50) # or just 0
+    board = cv.aruco.GridBoard_create(M, N, len, gap, dict)
+    dim = (int(1024 * (M*len + (M+1)*gap)), int(1024 * (N*len + (N+1)*gap)))
+    img = board.draw(dim, marginSize=margin, borderBits=1)
+    cv.imwrite(output_filename, img); # save img to the outfile
+    if verbose: # show the generated grid board
+      cv.imshow(output_filename, img)
+      cv.waitKey(0)
 
 if __name__ == "__main__":
     main()
