1ad81d652a897894bbdc4fa12f15bcc631726736
Henry <henry@rhom.me>
2021-11-14 15:43:41 -0800

    Resolved the aruco frame

--- aruco/src/ArucoNode.cpp
+++ aruco/src/ArucoNode.cpp
@@ -53,11 +53,11 @@ private:
 	Mat _distortion = cv::Mat(1, 5, CV_64F, data_distortion);
 
 	image_transport::ImageTransport _it;
-	image_transport::Publisher pub_debug_img;
+	image_transport::Publisher debug_img_pub;
 	image_transport::Subscriber _image0_sub, _image1_sub, _image2_sub, _image3_sub;
 	ros::Subscriber _cal1_sub;
 
-	tf2_ros::TransformBroadcaster br;
+	tf2_ros::TransformBroadcaster _br;
 	tf2_ros::Buffer _tf2_buffer;
 	tf2_ros::TransformListener _tf2_listener;
 	ros::Publisher _cam2marker_pub;
@@ -182,7 +182,7 @@ void ArucoPublisher::onFrame(const sensor_msgs::ImageConstPtr& msg) {
 		_cam2marker_pub.publish(cam2marker);
 		auto elapsed = ros::Time::now() - t0;
 
-		ROS_DEBUG("img took %u ms, %zd markers in cam %u; pose Q = [%.2f, %.2f, %.2f, %.2f] T = [%.3f, %.3f, %.3f]"
+		ROS_DEBUG("img took %u ms, %zd markers in cam%u; pose Q = [%.2f, %.2f, %.2f, %.2f] T = [%.3f, %.3f, %.3f]"
 				, elapsed.nsec/1000000
 				, markerIds.size(), camId
 				, cam2marker.pose.orientation.x
@@ -200,7 +200,7 @@ void ArucoPublisher::onFrame(const sensor_msgs::ImageConstPtr& msg) {
 			convert_frame_to_message(frame, img);
 			// preserve the timestamp from the image frame
 			img->header.stamp = msg->header.stamp;
-			pub_debug_img.publish(img);
+			debug_img_pub.publish(img);
 		}
 	} catch(cv_bridge::Exception& e) {
 		ROS_ERROR("Cannot get image");
@@ -212,7 +212,14 @@ void ArucoPublisher::onCam2Marker(const geometry_msgs::PoseStampedConstPtr& cam2
     geometry_msgs::PoseStamped quad2marker;
     try {
     	_tf2_buffer.transform(*cam2marker, quad2marker, "quad_link");
-
+		geometry_msgs::TransformStamped transformStamped;
+		transformStamped.header = quad2marker.header;
+		transformStamped.child_frame_id = "aruco";
+		transformStamped.transform.translation.x = quad2marker.pose.position.x;
+		transformStamped.transform.translation.y = quad2marker.pose.position.y;
+		transformStamped.transform.translation.z = quad2marker.pose.position.z;
+		transformStamped.transform.rotation = quad2marker.pose.orientation;
+		_br.sendTransform(transformStamped);
 		ROS_INFO(//"%u.%09u "
 				"%s pose in quad_link Q = [%.2f, %.2f, %.2f, %.2f] T = [%.3f, %.3f, %.3f]"
 				, cam2marker->header.frame_id.c_str()
@@ -275,7 +282,7 @@ void stringToDoubleArray(string data, double* values, unsigned int count, string
 ArucoPublisher::ArucoPublisher()
 : _nh("aruco"), _ph("~")
 , _it(_nh)
-, pub_debug_img(_it.advertise("debug", 1))
+, debug_img_pub(_it.advertise("debug", 1))
 , _image0_sub(_it.subscribe("quad0/image_raw", 1, &Self::onFrame, this))
 , _image1_sub(_it.subscribe("quad1/image_raw", 1, &Self::onFrame, this))
 , _image2_sub(_it.subscribe("quad2/image_raw", 1, &Self::onFrame, this))
